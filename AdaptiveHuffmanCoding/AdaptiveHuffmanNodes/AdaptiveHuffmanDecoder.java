255fb61265df260516f74709b4722a26b01457d4
= <bnjhope1@gmail.com>
2016-10-04 23:21:52 +0100

    updated decoder class

--- AdaptiveHuffmanCoding/AdaptiveHuffmanNodes/AdaptiveHuffmanDecoder.java
+++ AdaptiveHuffmanCoding/AdaptiveHuffmanNodes/AdaptiveHuffmanDecoder.java
@@ -1,7 +1,17 @@
 package AdaptiveHuffmanCoding.AdaptiveHuffmanNodes;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+
 /**
  * Created by bh59 on 03/10/16.
  */
 public class AdaptiveHuffmanDecoder {
+    AdaptiveHuffmanTree tree;
+
+    FileInputStream inputStream;
+
+    public String getNextBitsFromFile() throws IOException {
+        return Integer.toBinaryString(this.inputStream.read(););
+    }
 }
