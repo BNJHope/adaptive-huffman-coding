1b362208b8f3f6ee4b31913d640fd0d7586e021e
= <bnjhope1@gmail.com>
2016-10-06 12:19:22 +0100

    debugged part

--- AdaptiveHuffmanCoding/AdaptiveHuffmanCoding.java
+++ AdaptiveHuffmanCoding/AdaptiveHuffmanCoding.java
@@ -1,12 +1,15 @@
 package AdaptiveHuffmanCoding;
 
+import AdaptiveHuffmanCoding.AdaptiveHuffmanNodes.ParentDoesNotMatchChildException;
+
+import java.io.FileNotFoundException;
+
 public class AdaptiveHuffmanCoding {
 
     public static void main(String[] args){
         String readFilename = args[0];
         AdaptiveHuffmanEncoder encoder = new AdaptiveHuffmanEncoder(Integer.parseInt(args[1]));
         encoder.encode(readFilename);
-        AdaptiveHuffmanDecoder decoder = new AdaptiveHuffmanDecoder();
         System.out.println("Process complete");
     }
 
